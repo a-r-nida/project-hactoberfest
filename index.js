c6737fdf32e05a8cb5ee4fdc257feecfc0b48c92
sleepy81k <nabilnico@gmail.com>
2020-10-04 17:29:23 +0700

    git commit -b "Add My Name to Contruburing list"

---
+++ index.js
@@ -0,0 +1,10 @@
+// membuat perhitungan panjang dan lebar
+
+var A = prompt("Masukkan Panjang");
+var B = prompt("Masukkan Lebabr");
+
+const hitung = (A, B) => {
+  return A * B;
+};
+
+console.log(hitung(A, B));
